53e362f175dfb31ee5b609c3aecb20f06f5a60b8
maryamzeea <maryamzeea@icloud.com>
2025-07-04 16:53:53 +0500

    Space Complexity

---
+++ space_complexity.py
@@ -0,0 +1,20 @@
+# #SPACECOMLEXITY
+import sys
+
+
+# import tracemalloc
+# tracemalloc.start()
+# def sum_of_n(n):
+#     sum = 0
+#     for i in range(1,n+1):
+#         sum += i
+#     return sum
+#     # return n * (n + 1) // 2
+#
+# X=sum_of_n(100000)
+# print(X)
+# print(f"{tracemalloc.get_traced_memory()} bytes")
+#
+# tracemalloc.stop()
+
+print(sys.getsizeof("Maryam"))
