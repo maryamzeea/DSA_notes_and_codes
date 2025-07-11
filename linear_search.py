bd0059d6e0fd302b325f9851e22238c5436c7d21
maryamzeea <maryamzeea@icloud.com>
2025-07-11 23:43:43 +0500

    Linear Search

---
+++ linear_search.py
@@ -0,0 +1,26 @@
+#SEARCHING IN ARRAY
+import array as arr
+# x = int(input("Enter a number: "))
+# A = arr.array("i", [41, 26, 37, 49, 52])
+#
+# def linear_search(arr, x):
+#     for i in range(len(arr)):
+#         if arr[i] == x:
+#             return f"{x} is in the index {i}"
+#     return f"{x} is not in the list"
+# searching = linear_search(A,x)
+# print(searching)
+
+# l = list(map(int, input("Enter a list:").split()))
+# A = arr.array("i",l)
+#
+# def linear_search(arr, x):
+#     for i in range(len(arr)):
+#         if l[i] == x:
+#             return f"{x} is at index {i}"
+#     return f"{x} is at another index"
+#
+# search = linear_search(A, 30)
+# print(search)
+
+
